2c92cf1566cd2f78848067864ceacd5ec4eb8750
qinenergy <qwang@student.ethz.ch>
2017-02-18 23:46:15 +0100

    Update 27.Remove Element.cpp

--- 27.Remove Element.cpp
+++ 27.Remove Element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int i(0), j(0);
-        for(int i=0;i<nums.size();i++){
+        for(i=0;i<nums.size();i++){
             if(nums[i]!=val){
                 nums[j]=nums[i];
                 j++;
