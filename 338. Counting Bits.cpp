177bce2c4ca5eb3ee43b51aaf732eed5cd04f87b
qinenergy <qwang@student.ethz.ch>
2017-02-24 00:19:22 +0100

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -1,3 +1,14 @@
+class Solution1 {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res(num+1, 0);
+        for(int i=1; i<=num; i++){
+            res[i] = res[i&(i-1)]+1;
+        }
+        return res;
+    }
+};
+
 class Solution2 {
 public:
     vector<int> countBits(int num) {
