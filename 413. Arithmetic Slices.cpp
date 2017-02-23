7828aa388da5b56172c8850cfc5c9176cea280ec
qinenergy <qwang@student.ethz.ch>
2017-02-23 23:49:55 +0100

    Update 413. Arithmetic Slices.cpp

--- 413. Arithmetic Slices.cpp
+++ 413. Arithmetic Slices.cpp
@@ -1,3 +1,17 @@
+class Solution1 {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int n = A.size();
+        if(n<3) return 0;
+        vector<int> mark(n, 0);
+        for(int i=2; i<n; i++){
+            if(A[i]-A[i-1]==A[i-1]-A[i-2])
+                mark[i] = mark[i-1]+1;
+        }
+        return accumulate(mark.begin(),mark.end(),0);
+    }
+};
+
 class Solution2 {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
@@ -16,3 +30,4 @@ public:
         return count;
     }
 };
+
