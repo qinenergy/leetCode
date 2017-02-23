d66f2a962ebd689d82f0e58b732680d31422fb08
Squids Zyd <Squids Zyd>
2017-02-23 21:32:03 +0800

    413. Arithmetic Slices

---
+++ 413. Arithmetic Slices.cpp
@@ -0,0 +1,9 @@
+class Solution2 {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int len = A.size();
+        if(len <= 2)	return 0;
+        int count(0), delta, start(0), end(1);
+        
+    }
+};
