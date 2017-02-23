110608d68a2191795cbc3b964459d923701afbdd
qinenergy <qwang@student.ethz.ch>
2017-02-24 00:11:26 +0100

    Update 513. Find Bottom Left Tree Value.cpp

--- 513. Find Bottom Left Tree Value.cpp
+++ 513. Find Bottom Left Tree Value.cpp
@@ -5,6 +5,21 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+class Solution1 {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            root = q.front();
+            q.pop();
+            if(root->right) q.push(root->right);
+            if(root->left) q.push(root->left);
+        }
+        return root->val;
+    }
+};
+
 class Solution2 {
 public:
 	int findBottomLeftValue(TreeNode* root) {
