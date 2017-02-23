9054605cc61c1e1021c467b02133b158ee973192
qinenergy <qwang@student.ethz.ch>
2017-02-24 00:30:02 +0100

    Update 419.Battleships in a Board.cpp

--- 419.Battleships in a Board.cpp
+++ 419.Battleships in a Board.cpp
@@ -1,3 +1,17 @@
+class Solution1 {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int res = 0;
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                if(board[i][j]=='X' && (i==0 ||board[i-1][j]=='.') && (j==0 || board[i][j-1]=='.'))
+                    res++;
+            }
+        }
+        return res;
+    }
+};
+
 class Solution2 {
 public:
     int countBattleships(vector<vector<char>>& board) {
