3945c936b3ea387831b3203920b4d0d68d52d7e7
qinenergy <qwang@student.ethz.ch>
2017-02-24 20:31:34 +0100

    Update 7. Reverse Integer.cpp

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,3 +1,20 @@
+class Solution1 {
+public:
+    int reverse(int x) {
+        long long y = 0;
+        int absx = abs(x);
+        while (absx>0){
+            y = 10*y + absx%10;
+            absx /= 10;
+        }
+        if(x<0) y = -y;
+        if(y<INT_MIN || y>INT_MAX)
+            return 0;
+        else
+            return (int)y;
+    }
+};
+
 class Solution2 {
 public:
     int reverse(int x) {
