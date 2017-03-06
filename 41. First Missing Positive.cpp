f9f62a10a14c2c8f9408dca3a43bd6aaa0f87770
qinenergy <qwang@student.ethz.ch>
2017-03-06 19:25:40 +0100

    Update 41. First Missing Positive.cpp

--- 41. First Missing Positive.cpp
+++ 41. First Missing Positive.cpp
@@ -1,3 +1,27 @@
+class Solution1 {
+private:
+    int swapMe(vector<int>& nums, int i, int depth){
+        if(nums[i]<=0 || nums[i]>nums.size() || nums[i] == i+1 ||depth > nums.size()){
+            return 1;
+        }
+        swapMe(nums, nums[i]-1, ++depth);
+        swap(nums[i], nums[nums[i]-1]);
+        return 1;
+    }
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        if (nums.size() == 0) return 1;
+        for(int i=0; i<nums.size(); i++)
+            if(nums[i]>0 && nums[i]<=nums.size()) 
+                swapMe(nums, i, 1);
+        for(int i=0; i<nums.size(); i++)
+            if(nums[i]<=0 || nums[i]>nums.size() || nums[i]!=i+1)
+                return i+1;
+        int max = *max_element(nums.begin(), nums.end());
+        return max + 1;
+    }
+};
+
 class Solution2 {
 public:
     int firstMissingPositive(vector<int>& nums) {
