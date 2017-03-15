f61fee203cf6d0f5f28d7fc78b2fa4010a18210e
qinenergy <qwang@student.ethz.ch>
2017-03-15 11:09:45 +0100

    Update 62. Unique Paths.cpp

--- 62. Unique Paths.cpp
+++ 62. Unique Paths.cpp
@@ -1,3 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int res[m][n];
+        for(int i=0; i<m; i++)
+            res[i][0] = 1;
+        for(int i=0; i<n; i++)
+            res[0][i] = 1;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++)
+                res[i][j] = res[i-1][j] + res[i][j-1];
+        }
+        return res[m-1][n-1];
+    }
+};
 class Solution2 {
 public:
     int uniquePaths(int m, int n) {
