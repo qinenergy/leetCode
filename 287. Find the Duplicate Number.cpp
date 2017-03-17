fc7fb62381b910163f63561f8e0f532b89165b0a
Squids Zyd <Squids Zyd>
2017-03-17 17:51:13 +0800

    #287

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -21,7 +21,19 @@ public:
 class Solution2 {
 public:
     int findDuplicate(vector<int>& nums) {
-        cout << "No idea right now T_T" << endl;
-    	return 0;	
+    	int slow = 0, fast = 0;	
+    	while(1){
+    		slow = nums[slow];
+    		fast = nums[nums[fast]];
+    		if(slow == fast)
+    			break;
+    	}
+    	int finder = 0;
+    	while(1){
+    		finder = nums[finder];
+    		slow = nums[slow];
+    		if(slow == finder)
+    			return slow;
+    	}
     }
 };
