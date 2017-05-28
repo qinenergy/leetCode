7e53163fa9a0b50252203ec75c475443f547d01a
Squids Zyd <Squids Zyd>
2017-05-28 15:49:28 +0800

    replace binary compare function with lambda

--- 179. Largest Number.cpp
+++ 179. Largest Number.cpp
@@ -1,10 +1,5 @@
 class Solution2 {
 public:
-    static bool biggerThan(string& num1, string& num2){
-        string s1 = num1 + num2;
-        string s2 = num2 + num1;
-        return s1 > s2;
-    }
     string largestNumber(vector<int>& nums) {
        int len = nums.size();
        vector<string> str_nums;
@@ -14,10 +9,11 @@ public:
            all_zero = all_zero && (num == 0);
        }
        if(all_zero) return "0";
-       sort(str_nums.begin(), str_nums.end(), biggerThan);
+       sort(str_nums.begin(), str_nums.end(),
+            [](const string& s1, const string& s2){ return s1 + s2 > s2 + s1; });
        string rst = "";
        for(const auto& str_num : str_nums) rst += str_num;
        if(rst.empty()) return 0;
        return rst;
    }
-;
+};
