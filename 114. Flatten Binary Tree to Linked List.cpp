e08c362151f5c5ac86803d008255661a8645d9f3
Squids Zyd <squidroidnxs@gmail.com>
2017-05-31 22:00:00 +0800

    another solution using morris like traversal

--- 114. Flatten Binary Tree to Linked List.cpp
+++ 114. Flatten Binary Tree to Linked List.cpp
@@ -28,4 +28,22 @@ public:
             }
         }
     }
+
+    // Solution 2. By modifying the right-most treenode of left subtree
+    void flatten(TreeNode *root) {
+        while(root){
+            if(root->left == NULL)
+                root = root->right;
+            else {
+                if(root->right){
+                    TreeNode *l = root->left;
+                    while(l->right) l = l->right;
+                    l->right = root->right;
+                }
+                root->right = root->left;
+                root->left = NULL;
+                root = root->right;
+            }
+        }
+    }
 };
